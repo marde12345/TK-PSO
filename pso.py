f919ab640b446a95d0c4c3dd33a53620aed35ae2
marde12345 <mardefasma123up@gmail.com>
2018-11-02 03:24:09 +0700

    Fitur buat generate png tiap generasi

--- pso.py
+++ pso.py
@@ -4,32 +4,33 @@ import matplotlib.pyplot as plt
 
 def main():
     #Variables
-    n = 40
+    n = 5
     num_variables = 2
 
-    #Crear arreglos
+    #Create Array
     a = np.empty((num_variables, n))
     v = np.empty((num_variables, n))
     Pbest = np.empty((num_variables, n))
     Gbest = np.empty((1, 2))
     r = np.empty((n))
 
-    #Llenar arreglos
+    #Random Array
     for i in range(0, num_variables):
         for j in range(0, n):
-            Pbest[i][j] = rand.randint(-20, 20)
+            Pbest[i][j] = rand.randint(-10, 10)
             a[i][j] = Pbest[i][j]
             v[i][j] = 0
     
-    #Llenar r
+    #r
     for i in range(0, n):
         r[i] = fitness(a[0][i], a[1][i])
 
-    #Ordenar elementos de Pbest
-    Ordenamiento_Burbuja(Pbest, r, n)
+    #Sort Pbest
+    Sort(Pbest, r, n)
 
     Gbest[0][0] = Pbest[0][0]
     Gbest[0][1] = Pbest[1][0]
+    print('gbest ak ',Gbest)
 
     generation = 0
 
@@ -38,20 +39,20 @@ def main():
     ax = fig.add_subplot(111)
     ax.grid(True)
     
-    while(generation < 100):
+    while(generation < 50):
         for i in range(n):
-            #Obtener Pbest
+            #Pbest
             if(fitness(a[0][i], a[1][i]) < fitness(Pbest[0][i], Pbest[1][i])):
                 Pbest[0][i] = a[0][i]
                 Pbest[1][i] = a[1][i]
-            #Obtener Gbest
+            #Gbest
             if(fitness(Pbest[0][i], Pbest[1][i]) < fitness(Gbest[0][0], Gbest[0][1])):
                 Gbest[0][0] = Pbest[0][i]
                 Gbest[0][1] = Pbest[1][i]
-            #Calcular Velocidad
-            Vector_Velocidad(n, a, Pbest, Gbest, v)
+            #Velocity
+            Vector_velocity(n, a, Pbest, Gbest, v)
         generation = generation + 1
-        print ('Generacion: ' + str(generation) + ' - - - Gbest: ' +str(Gbest))
+        print ('Generation: ' + str(generation) + ' - - - Gbest: ' +str(Gbest))
 
         line1 = ax.plot(a[0], a[1], 'r+')
         line2 = ax.plot(Gbest[0][0], Gbest[0][1], 'g*')
@@ -60,7 +61,7 @@ def main():
         ax.set_ylim(-10, 10)
         
         fig.canvas.draw()
-
+        fig.savefig('./evol_conc_v'+str(generation)+'.png')
         ax.clear()
         ax.grid(True)
 
@@ -68,37 +69,37 @@ def main():
     print ('Gbest: ')
     print (Gbest)
 
-def Vector_Velocidad(n, a, Pbest, Gbest, v):
+def Vector_velocity(n, a, Pbest, Gbest, v):
     for i in range(n):
-        #Velocidad en X
+        #X
         v[0][i] = 0.7 * v[0][i] + (Pbest[0][i] - a[0][i]) * rand.random() * 1.47 + (Gbest[0][0] - a[0][i]) * rand.random() * 1.47
         a[0][i] = a[0][i] + v[0][i]
-        #Velocidad en Y
+        #Y
         v[1][i] = 0.7 * v[1][i] + (Pbest[1][i] - a[1][i]) * rand.random() * 1.47 + (Gbest[0][1] - a[1][i]) * rand.random() * 1.47
         a[1][i] = a[1][i] + v[1][i]
 
 def fitness(x, y):
-        return 100 * ((y - (x**2))**2) + ((1 - (x**2))**2)
-        #100 * ((y - (x**2))**2) + ((1 - (x**2))**2)
+        return 100 * ((y - (x**2))**2) + ((1 - (x))**2)
+        #100 * (y-x^2)^2+(1-x^2)^2
 
-def Ordenamiento_Burbuja(Pbest, r, n):
-    #Ordenamiento burbuja
+def Sort(Pbest, r, n):
+    #Bubble Sort
     for i in range(1, n):
         for j in range(0, n - 1):
             if r[j] > r[j + 1]:
-                #Ordenar el fitness
+                #fitness
                 tempRes = r[j]
                 r[j] = r[j + 1]
                 r[j + 1] = tempRes
 
-                #Ordenar las X, Y
+                #X, Y
                 tempX = Pbest[0][j]
                 Pbest[0][j] = Pbest[0][j + 1]
                 Pbest[0][j + 1] = tempX
 
+                #Pbest
                 tempY = Pbest[1][j]
                 Pbest[1][j] = Pbest[1][j + 1]
                 Pbest[1][j + 1] = tempY
 
-if '__main__' == main():
-    main()
+main()
